aa025759d5027d51ca37fecaf420409dbde24c3a
Collin J. Sutton <collin.sutton@gmail.com>
2010-02-08 15:57:32 -0800

    Documented strutils header file; strstrip(s) is now an inline function

--- include/support/strutils.h
+++ include/support/strutils.h
@@ -5,28 +5,67 @@
 extern "C"
 {
 #endif
+#include <sys/types.h>          /* for size_t */
 
-  /** Concatenate strings `a' and `b'. Automagically allocates memory
-   * if passed a NULL dest or n, or a (*n) of 0.
+  /** Concatenate strings `a' and `b', storing the result in a
+   * separate location.  Automagically allocates memory if passed a
+   * NULL dest or n, or a (*n) of 0.
    *
-   * @param	dest	Destination in which to store result.
+   * @param dest Pointer to destination pointer in which to store
+   * result.  I
+   *
+   *
+   * @param a A string.
+   *
+   * @param b Another string, to be appended to @p a.
+   *
+   * @return String pointer to the concatenation of @p a and @p b. If
+   * @p dest is @c NULL, this will be allocated with @f malloc and
+   * must be freed by the user.
    */
   char*
   stalins_cat(char** dest, size_t *n, const char* a, const char* b);
 
 
+  /** Find the first non-space character in a string.
+   *
+   * @param in An input string.
+   *
+   * @return A pointer to the first non-space character in @p in.
+   */
   char*
-  strchomp(const char*);
+  strchomp(const char* in);
 
+  /** Get a copy of a string, sans trailing whitespace.
+   *
+   * @param in Input string.
+   *
+   * @return A newly-allocated string, containing a copy of @p in sans
+   * trailing whitespace.
+   */
   char*
-  strchug(const char*);
-
-#define strstrip(s) strchug(strchomp(s))
+  strchug(const char* in);
 
 
-  /** An alternative to asprintf.
+  /** Convenience wrapper for both strchomp and strchug.  Returns a
+   * new copy of string @p in with all whitespace removed.
+   *
+   * @param in The string to strip.
+   *
+   * @return A newly-allocated string.
+   */
+  __inline char*
+  strstrip(const char* in)
+  {
+    return strchug(strchomp(in));
+  }
+
+  /** Alternative to @f asprintf that returns the created string.
    *
    * \param	format	Format string.
+   *
+   * \param	...	<code>printf</code>-style arguments for the
+   * format string.
    *
    * \returns	A string.
    */
