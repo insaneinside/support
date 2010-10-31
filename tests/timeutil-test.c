bbc4f8868e3a6a1967a868e8519c255177f66bfa
Collin J. Sutton <collin.sutton@gmail.com>
2010-10-31 09:45:45 -0700

    Fixed timeutil include name re: d7d83

--- tests/timeutil-test.c
+++ tests/timeutil-test.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include "timeutil.h"
+#include <support/timeutil.h>
 #include <support/scalar.h>
 
 /** Fill time by doing some expensive calculations.
