91f676db778637d1c4c834588091d4726341c322
Collin J. Sutton <collin.sutton@gmail.com>
2011-06-20 00:23:53 -0700

    SArray: changed scope to spt::SArray;  fixed delete => delete[]; other cleanups

--- include/support/SArray.hh
+++ include/support/SArray.hh
@@ -4,65 +4,99 @@
 #include <cstddef>
 #include <utility>
 
-template < typename _Tp >
-struct SArray
+namespace spt
 {
-  _Tp* item;
-  size_t size;
-
-  inline SArray()
-    : item ( NULL ),
-      size ( 0 )
+  /** Simple, static array with basic memory management.
+   */
+  template < typename _Tp >
+  struct SArray
   {
-  }
+    _Tp* item;
+    size_t size;
 
-  inline SArray(SArray&& sa)
-    : item ( std::move(sa.item) ),
-      size ( std::move(sa.size) )
-  {
-    sa.item = NULL;
-    sa.size = 0;
-  }
+#if __cplusplus > 199711L || defined(__GXX_EXPERIMENTAL_CXX0X__)
+    /**@name Deleted functions.
+     *This class is non-copyable.
+     *@{
+     */
+    SArray(const SArray&) = delete;
+    SArray& operator=(const SArray&) = delete;
+    /**@*/
 
-  inline SArray(SArray& sa)
-    : item ( sa.item ),
-      size ( sa.size )
-  {
-    sa.item = NULL;
-    sa.size = 0;
-  }
+    inline SArray()
+      : item ( NULL ),
+	size ( 0 )
+    {
+    }
+
+    inline SArray(SArray&& sa)
+      : item ( std::move(sa.item) ),
+	size ( std::move(sa.size) )
+    {
+      sa.item = NULL;
+      sa.size = 0;
+    }
+#endif
+
+    inline SArray(SArray& sa)
+      : item ( sa.item ),
+	size ( sa.size )
+    {
+      sa.item = NULL;
+      sa.size = 0;
+    }
 
 
-  inline SArray(size_t s)
-    : item ( new _Tp[s] ),
-      size ( s )
-  {
-  }
+    inline SArray(size_t s)
+      : item ( new _Tp[s] ),
+	size ( s )
+    {
+    }
 
-  inline ~SArray()
-  {
-    if ( item )
-      delete item;
-  }
+    inline ~SArray()
+    {
+      if ( item )
+	delete[] item;
+    }
 
-  inline SArray&
-  operator = (SArray&& sa)
-  {
-    item = sa.item;
-    size = sa.size;
+#if __cplusplus > 199711L || defined(__GXX_EXPERIMENTAL_CXX0X__)
+    inline SArray&
+    operator = (SArray&& sa)
+    {
+      item = std::move(sa.item);
+      size = std::move(sa.size);
 
-    sa.item = NULL;
-    sa.size = 0;
+      sa.item = NULL;
+      sa.size = 0;
 
-    return *this;
-  }
+      return *this;
+    }
+#endif
 
-  inline _Tp&
-  operator[](size_t n)
-  {
-    return item[n];
-  }
+    inline SArray&
+    operator = (SArray& sa)
+    {
+      item = std::move(sa.item);
+      size = std::move(sa.size);
 
-};
+      sa.item = NULL;
+      sa.size = 0;
 
+      return *this;
+    }
+
+
+    inline _Tp&
+    operator[](size_t n)
+    {
+      return item[n];
+    }
+
+    inline const _Tp&
+    operator[](size_t n) const
+    {
+      return item[n];
+    }
+  };
+}
 #endif	/* defined(SArray_hh) */
