1e2a674ec994fd2be7fc1dd12bd77e3d6b06ce2d
Collin J. Sutton <collin.sutton@gmail.com>
2012-07-08 22:52:55 -0700

    SArray: Several minor fixes/cleanups
      * Added a `value_type' typedef
    
      * Moved C++11 methods to a single #ifdef block
    
      * Added `noexcept' (with proper C++11 #ifdef) to default constructor
    
      * Moved memory allocation/deallocation calls out of c/d-tors and into
        their own methods.

--- include/support/SArray.hh
+++ include/support/SArray.hh
@@ -6,15 +6,69 @@
 
 namespace spt
 {
-  /** Simple, static array with basic memory management.
+  /** Simple, statically-sized array with basic memory management
+   *
+   * @tparam _Tp Element type.
    */
   template < typename _Tp >
   struct SArray
   {
-    _Tp* item;
+    typedef _Tp value_type;
+
+    value_type* item;
     size_t size;
 
+
+    static inline value_type*
+    alloc(size_t count)
+    {
+      return static_cast<value_type*>(::operator new[](count * sizeof(_Tp)));
+    }
+
+    static inline void
+    dealloc(value_type* ptr)
+    {
+      ::operator delete[](ptr);
+    }
+
+    inline SArray()
 #if __cplusplus > 199711L || defined(__GXX_EXPERIMENTAL_CXX0X__)
+      noexcept
+#endif
+      : item ( NULL ),
+	size ( 0 )
+    {
+    }
+
+
+    inline SArray(size_t s)
+      : item ( s > 0 ? SArray::alloc(s) : NULL ),
+	size ( s )
+    {
+    }
+
+
+    inline ~SArray()
+    {
+      if ( item )
+	{
+	  dealloc(item);
+	  item = NULL;
+	}
+    }
+
+
+#if __cplusplus > 199711L || defined(__GXX_EXPERIMENTAL_CXX0X__)
+    /** Move constructor. */
+    inline SArray(SArray&& sa)
+      : item ( std::move(sa.item) ),
+	size ( std::move(sa.size) )
+    {
+      sa.item = NULL;
+      sa.size = 0;
+    }
+
+
     /**@name Deleted functions.
      *This class is non-copyable.
      *@{
@@ -23,43 +77,8 @@ namespace spt
     SArray& operator=(const SArray&) = delete;
     /**@*/
 
-    inline SArray()
-      : item ( NULL ),
-	size ( 0 )
-    {
-    }
 
-    inline SArray(SArray&& sa)
-      : item ( std::move(sa.item) ),
-	size ( std::move(sa.size) )
-    {
-      sa.item = NULL;
-      sa.size = 0;
-    }
-#endif
-
-    inline SArray(SArray& sa)
-      : item ( sa.item ),
-	size ( sa.size )
-    {
-      sa.item = NULL;
-      sa.size = 0;
-    }
-
-
-    inline SArray(size_t s)
-      : item ( new _Tp[s] ),
-	size ( s )
-    {
-    }
-
-    inline ~SArray()
-    {
-      if ( item )
-	delete[] item;
-    }
-
-#if __cplusplus > 199711L || defined(__GXX_EXPERIMENTAL_CXX0X__)
+    /** Rvalue assignment operator. */
     inline SArray&
     operator = (SArray&& sa)
     {
@@ -72,19 +91,6 @@ namespace spt
       return *this;
     }
 #endif
-
-    inline SArray&
-    operator = (SArray& sa)
-    {
-      item = std::move(sa.item);
-      size = std::move(sa.size);
-
-      sa.item = NULL;
-      sa.size = 0;
-
-      return *this;
-    }
-
 
     inline _Tp&
     operator[](size_t n)
